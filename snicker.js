5a4d6b4b36dfed2ca35359f2e00fa77605185f92
Vanice Yee <vanice.yee@gmail.com>
2016-11-08 10:51:40 -0500

    fixed

--- snicker.js
+++ snicker.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   var $counter = $('#counter');
   var $count = 0;
   var $start = $('#start');
-  var timeLeft = 45;
+  var timeLeft = 60;
   var snicker = $('.snicker');
   var $candyJar = $('#candyJar');
   var stop;
